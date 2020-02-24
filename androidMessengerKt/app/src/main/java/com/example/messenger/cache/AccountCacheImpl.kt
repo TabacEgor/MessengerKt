917047de23dfe1dc4bf68e6edc50d9f9b20e1321
Egor Tabac <goshatabake@gmail.com>
2020-02-24 21:29:30 +0200

    Cache layer / logout, getCurrentAccount, saveAccount

--- androidMessengerKt/app/src/main/java/com/example/messenger/cache/AccountCacheImpl.kt
+++ androidMessengerKt/app/src/main/java/com/example/messenger/cache/AccountCacheImpl.kt
@@ -1,6 +1,7 @@
 package com.example.messenger.cache
 
 import com.example.messenger.data.account.IAccountCache
+import com.example.messenger.domain.account.AccountEntity
 import com.example.messenger.domain.type.Either
 import com.example.messenger.domain.type.None
 import com.example.messenger.domain.type.Failure
@@ -14,5 +15,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun saveToken(token: String): Either<Failure, None> {
         return prefsManager.saveToken(token)
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
