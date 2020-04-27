c7e7db9283fda39da148bda3f6b30c3675a35ab2
Egor Tabac <goshatabake@gmail.com>
2020-04-27 15:05:57 +0300

    Remote layer / Editing user

--- androidMessengerKt/app/src/main/java/com/example/messenger/remote/account/AccountRemoteImpl.kt
+++ androidMessengerKt/app/src/main/java/com/example/messenger/remote/account/AccountRemoteImpl.kt
@@ -7,7 +7,9 @@ import com.example.messenger.domain.type.None
 import com.example.messenger.domain.type.Failure
 import com.example.messenger.remote.core.Request
 import com.example.messenger.remote.service.IApiService
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.HashMap
 
 class AccountRemoteImpl @Inject constructor(
     private val request: Request,
@@ -34,6 +36,19 @@ class AccountRemoteImpl @Inject constructor(
 
     override fun updateToken(userId: Long, token: String, oldToken: String): Either<Failure, None> {
         return request.make(service.updateToken(createUpdateTokenMap(userId, token, oldToken))) { None() }
+    }
+
+    override fun editUser(
+        userId: Long,
+        email: String,
+        name: String,
+        password: String,
+        status: String,
+        token: String,
+        image: String
+    ): Either<Failure, AccountEntity> {
+        return request.make(service.editUser(createUserEditMap(userId, email, name, password, status,
+            token, image))) { it.user }
     }
 
     private fun createRegisterMap(
@@ -73,6 +88,31 @@ class AccountRemoteImpl @Inject constructor(
         map.put(IApiService.PARAM_USER_ID, userId.toString())
         map.put(IApiService.PARAM_TOKEN, token)
         map.put(IApiService.PARAM_OLD_TOKEN, oldToken)
+        return map
+    }
+
+    private fun createUserEditMap(
+        id: Long,
+        email: String,
+        name: String,
+        password: String,
+        status: String,
+        token: String,
+        image: String
+    ): Map<String, String> {
+        val map = HashMap<String, String>()
+        map.put(ApiService.PARAM_USER_ID, id.toString())
+        map.put(ApiService.PARAM_EMAIL, email)
+        map.put(ApiService.PARAM_NAME, name)
+        map.put(ApiService.PARAM_PASSWORD, password)
+        map.put(ApiService.PARAM_STATUS, status)
+        map.put(ApiService.PARAM_TOKEN, token)
+        if (image.startsWith("../")) {
+            map.put(ApiService.PARAM_IMAGE_UPLOADED, image)
+        } else {
+            map.put(ApiService.PARAM_IMAGE_NEW, image)
+            map.put(ApiService.PARAM_IMAGE_NEW_NAME, "user_${id}_${Date().time}_photo")
+        }
         return map
     }
 }
