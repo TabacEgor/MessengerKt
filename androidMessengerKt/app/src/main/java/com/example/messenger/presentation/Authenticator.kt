b2a1efade0df9080ad1c1b5a885d3079244eadfa
Egor Tabac <goshatabake@gmail.com>
2020-05-01 14:03:50 +0300

    Presentation layer / fix: auth caching

--- androidMessengerKt/app/src/main/java/com/example/messenger/presentation/Authenticator.kt
+++ androidMessengerKt/app/src/main/java/com/example/messenger/presentation/Authenticator.kt
@@ -1,12 +1,15 @@
 package com.example.messenger.presentation
 
-import com.example.messenger.cache.SharedPrefsManager
+import com.example.messenger.domain.account.CheckAuth
+import com.example.messenger.domain.type.None
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class Authenticator @Inject constructor(
-    val sharedPrefsManager: SharedPrefsManager
+    val checkAuth: CheckAuth
 ) {
-    fun userLoggedIn() = sharedPrefsManager.containsAnyAccount()
+    fun userLoggedIn(body: (Boolean) -> Unit) = checkAuth(None()) {
+        it.either({ body(false) }, { body(it) })
+    }
 }
