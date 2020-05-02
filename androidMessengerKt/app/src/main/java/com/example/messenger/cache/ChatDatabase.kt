f6bc1ffaa6325f87b261b85b4df9b909e6e899e5
Egor Tabac <goshatabake@gmail.com>
2020-05-02 11:46:47 +0300

    Cache layer / Messages

--- androidMessengerKt/app/src/main/java/com/example/messenger/cache/ChatDatabase.kt
+++ androidMessengerKt/app/src/main/java/com/example/messenger/cache/ChatDatabase.kt
@@ -5,12 +5,15 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.messenger.cache.friends.FriendsDao
+import com.example.messenger.cache.messages.MessagesDao
 import com.example.messenger.domain.friends.FriendEntity
+import com.example.messenger.domain.messages.MessageEntity
 
-@Database(entities = [FriendEntity::class], version = 2, exportSchema = false)
+@Database(entities = [FriendEntity::class, MessageEntity::class], version = 5, exportSchema = false)
 abstract class ChatDatabase : RoomDatabase() {
 
     abstract val friendsDao: FriendsDao
+    abstract val messagesDao: MessagesDao
 
     companion object {
         @Volatile
